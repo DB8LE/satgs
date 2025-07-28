27974bede1c863eb2222c66bce13e58d7259e251
DB8LE <db8le@darc.de>
2025-07-28 12:37:37 +0200

    Set up automatic linting across multiple versions

---
+++ noxfile.py
@@ -0,0 +1,8 @@
+import nox_poetry
+
+# Simple function to lint across multiple python versions
+@nox_poetry.session(python=["3.10", "3.11", "3.12", "3.13"])
+def lint(session):
+    print(session.python)
+    session.run("poetry", "install", "--no-root", external=True)
+    session.run("poetry", "run", "ruff", "check", ".", external=True)
